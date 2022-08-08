1df8d35bc324da518f738772db3016db5ba2795b
Pavel Vik <jviktes@gmail.com>
2022-08-08 10:28:30 +0200

    reorganisation code
    devide widget and backend code

--- widgets/filereader-widget/src/backend.js
+++ widgets/filereader-widget/src/backend.js
@@ -15,10 +15,101 @@ module.exports = function(r) {
     });
 });
 }
+
 module.exports = async function(r) {
     r.register('filesAPI', 'GET', (req, res, next, helper) => {
+    //const moment = require('moment');
     console.log("filesAPI");
     const testFolder = '/var/log/cloudify/archive/tentant-AA1/';    
+    
+    const processedData = data => {
+        console.log("processedData backroud code:");
+        let outputData = [];
+        console.log(data);
+
+        //const moment = require('moment');
+
+
+        console.log("before foreach");
+        for (const [key, value] of Object.entries(data)) {
+
+            console.log(`${key}: ${value}`);
+            //console.log(value);
+
+            let _res = [];
+            let _fileName = key; //<VM-name>-<ACT-class>-<ACT-set>-<timestamp>.log 
+
+            //funkce pro splitting:
+            let text = _fileName;
+            const myArray = text.split("-");
+            let _VM = myArray[0];
+            let _actClassFromFileName = myArray[1];
+            let _actSetFromFileName = myArray[2];
+            let _actTimeStampFromFileName = myArray[3].slice(0, -4); //YYYYMMDDhhmmss 
+
+            //let _testDateFormatted = moment(_actTimeStampFromFileName, 'YYYYMMDDhhmmss').format("YYYY-MM-DD hh:mm:ss");
+            let _testDateFormatted = _actTimeStampFromFileName;
+            console.log(_testDateFormatted);
+            
+            let _actual_value = [];
+            let _class = [];
+            let _code = [];
+            let _description= [];
+            let _expected_value= [];
+            let _name= [];
+            let _result= [];
+            let _testResultArray = [];
+            let _passedTestsCount = 0;
+            let _failedTestsCount = 0;
+
+            //prerovnani vysledku testu, podle pole result:
+
+            value.results.forEach(_testData => {
+                _actual_value = _testData.actual_value;
+                _class = _testData.class;
+                _code = _testData.code;
+                _description= _testData.description;
+                _expected_value= _testData.expected_value;
+                _name= _testData.name;
+                _result = _testData.result;
+                if (_testData.result.toString().toLowerCase() && _testData.result.toString().toLowerCase().indexOf("passed")!== -1) {
+                    _passedTestsCount++;
+                }
+                if (_testData.result.toString().toLowerCase() && _testData.result.toString().toLowerCase().indexOf("failed")!== -1) {
+                    _failedTestsCount++;
+                }
+
+                _testResultArray.push(
+                    {
+                        "actual_value": _actual_value,
+                        "class": _class,
+                        "code":  _code, 
+                        "description":  _description,
+                        "expected_value":  _expected_value,
+                        "name":  _name,
+                        "result": _result,
+                    }
+                );
+            });
+
+            outputData.push({
+                "fileName":_fileName, 
+                "virtualMachine": _VM,
+                "testDatum": _testDateFormatted,
+                "requestor": value.requestor,
+                "deployment_id":value.deployment_id,
+                "deployment_name":value.deployment_name, 
+                "passedTestsCount" : _passedTestsCount,
+                "failedTestsCount": _failedTestsCount,
+                "testResultArray":JSON.stringify(_testResultArray)
+            });
+        }
+        //console.log(outputData);
+        return outputData;
+
+    }
+    
+    
     let result ={};
 
     const folder = testFolder;
@@ -49,9 +140,15 @@ module.exports = async function(r) {
                 }
             });
             Promise.all(promises).then((_res) => {
-                console.log("data from files ready2go");
+                console.log("data from files ready2go:");
                 //console.log(result);
-                res.send(result);
+                let preparedData = processedData(result);
+
+                // prvni verze sortingu podle souboru:
+                preparedData.sort((a,b) => (a.fileName < b.fileName) ? 1 : ((b.fileName < a.fileName) ? -1 : 0));
+
+                console.log(preparedData);
+                res.send(preparedData);
             });
         });
 
--- widgets/filereader-widget/src/widget.tsx
+++ widgets/filereader-widget/src/widget.tsx
@@ -35,98 +35,9 @@ Stage.defineWidget({
         return params;
     },
 
-    processData(data) {
-
-        let outputData = [];
-
-        //console.log(data);
-        // toto bylo ok:
-        // for(let key of Object.keys(data)){
-        //     console.log(key);
-        // }
-
-        for (const [key, value] of Object.entries(data)) {
-
-            //console.log(`${key}: ${value}`);
-            //console.log(value);
-
-            let _res = [];
-            let _fileName = key; //<VM-name>-<ACT-class>-<ACT-set>-<timestamp>.log 
-
-            //funkce pro splitting:
-            let text = _fileName;
-            const myArray = text.split("-");
-            let _VM = myArray[0];
-            let _actClassFromFileName = myArray[1];
-            let _actSetFromFileName = myArray[2];
-            let _actTimeStampFromFileName = myArray[3].slice(0, -4); //YYYYMMDDhhmmss 
-
-            let _testDateFormatted = moment(_actTimeStampFromFileName, 'YYYYMMDDhhmmss').format("YYYY-MM-DD hh:mm:ss");;
-            console.log(_testDateFormatted);
-
-            let _actual_value = [];
-            let _class = [];
-            let _code = [];
-            let _description= [];
-            let _expected_value= [];
-            let _name= [];
-            let _result= [];
-            let _testResultArray = [];
-            let _passedTestsCount = 0;
-            let _failedTestsCount = 0;
-
-            //prerovnani vysledku testu, podle pole result:
-            _.each(value.results, _testData => {
-                _actual_value = _testData.actual_value;
-                _class = _testData.class;
-                _code = _testData.code;
-                _description= _testData.description;
-                _expected_value= _testData.expected_value;
-                _name= _testData.name;
-                _result = _testData.result;
-                if (_testData.result.toString().toLowerCase() && _testData.result.toString().toLowerCase().indexOf("passed")!== -1) {
-                    _passedTestsCount++;
-                }
-                if (_testData.result.toString().toLowerCase() && _testData.result.toString().toLowerCase().indexOf("failed")!== -1) {
-                    _failedTestsCount++;
-                }
-
-                _testResultArray.push(
-                    {
-                        "actual_value": _actual_value,
-                        "class": _class,
-                        "code":  _code, 
-                        "description":  _description,
-                        "expected_value":  _expected_value,
-                        "name":  _name,
-                        "result": _result,
-                    }
-                );
-            });
-
-            outputData.push({
-                "fileName":_fileName, 
-                "virtualMachine": _VM,
-                "testDatum": _testDateFormatted,
-                "requestor": value.requestor,
-                "deployment_id":value.deployment_id,
-                "deployment_name":value.deployment_name, 
-                "passedTestsCount" : _passedTestsCount,
-                "failedTestsCount": _failedTestsCount,
-                "testResultArray":JSON.stringify(_testResultArray)
-            });
-        }
-        //console.log(outputData);
-        return outputData;
-    },
     render(widget, data, error, toolbox) {
-        const _data = this.processData(data);
-        //console.log(pokusData);
-
-        //console.group(toolbox.getContext());
-
         const formattedData = {
-            items: _data,
+            items: data,
             deploymentId: 'test results'
         };
 
