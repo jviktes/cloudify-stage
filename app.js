c4cf068b0f8effebd0b1f34968f0c9f80b8e9d5b
Pavel Vik <jviktes@gmail.com>
2022-09-01 17:00:35 +0200

    added test node js server for getting external data

---
+++ app.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-console, no-process-exit */
+// @ts-nocheck File not migrated fully to TS
+const express = require('express');
+const app = express();
+
+const fs = require('fs/promises'); 
+const path = require('path');
+
+// Defining get request at '/' route
+app.get('/', function(req, res) {
+  console.log("root");
+  res.json({
+    number: 1
+  });
+});
+
+// Defining get request at '/array' route
+app.get('/array', function(req, res) {
+  console.log("array");
+  res.json([{
+      number: 1,
+      name: 'John',
+      gender: 'male'
+    },
+    {
+      number: 2,
+      name: 'Ashley',
+      gender: 'female'
+    },
+    {
+      number: 3,
+      name: 'Pavel',
+      gender: 'male'
+    },
+    {
+      number: 2,
+      name: 'Jana',
+      gender: 'female'
+    }
+  ]);
+});
+  
+// Setting the server to listen at port 3000
+app.listen(3000, function(req, res) {
+  console.log("Server is running at port 3000");
+});
